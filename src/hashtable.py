930725c98ad55c0eb4170215418ab33151b5c5c2
Edgar F <eflores.ef@gmail.com>
2020-02-01 10:25:14 -0800

    Implemented insert, retrieve, and remove. Collisions not yet handled

--- src/hashtable.py
+++ src/hashtable.py
@@ -7,7 +7,9 @@ class LinkedPair:
         self.value = value
         self.next = None
 
- 
+    # def __str__(self):
+    #     pass
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
@@ -21,6 +23,7 @@ class HashTable:
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
+
 
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
@@ -52,7 +55,15 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        new_item = LinkedPair(key, value)
+
+        address = self._hash_mod(key)
+
+        self.storage[address] = new_item
+
+        print(f'inserted: ({new_item.key}, {new_item.value})')
+        print(self.storage)
 
 
 
@@ -64,7 +75,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+      
+        address = self._hash_mod(key)
+        
+        self.storage.remove(self.storage[address])
+
+        print (f'removed: {self.storage[address]}')
+        print (self.storage)
+
 
 
     def retrieve(self, key):
@@ -75,7 +93,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        address = self._hash_mod(key)
+        
+        print(f'retrieved: {self.storage[address]}')
+        return self.storage[address]
+
 
 
     def resize(self):
@@ -87,32 +110,36 @@ class HashTable:
         '''
         pass
 
+hashbrown = HashTable(5)
+hashbrown.insert(3, 10)
+hashbrown.retrieve(3)
+hashbrown.remove(3)
 
 
-if __name__ == "__main__":
-    ht = HashTable(2)
+# if __name__ == "__main__":
+#     ht = HashTable(2)
 
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+#     ht.insert("line_1", "Tiny hash table")
+#     ht.insert("line_2", "Filled beyond capacity")
+#     ht.insert("line_3", "Linked list saves the day!")
 
-    print("")
+#     print("")
 
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test storing beyond capacity
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+#     # Test resizing
+#     old_capacity = len(ht.storage)
+#     ht.resize()
+#     new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test if data intact after resizing
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    print("")
+#     print("")
